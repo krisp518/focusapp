6ec93c144688db0bef569b2c8f58864809780f11
Ian Kim <eiankim518@gmail.com>
2024-04-08 02:00:21 -0700

    fully working image upload function that allows user to click on a button that allows them to upload image from gallery and will display said image in the center of the screen.

---
+++ app/src/main/java/com/example/focusapp2/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.focusapp2;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity{
+    //tickle me.
+
+    Button btnPickImage;
+    ImageView imageView;
+
+    ActivityResultLauncher<Intent> resultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPickImage = findViewById(R.id.btnPickImage);
+        imageView = findViewById(R.id.imageView);
+        registerResult();
+
+        btnPickImage.setOnClickListener(view -> pickImage());
+    }
+
+    private void pickImage(){
+        Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
+        resultLauncher.launch(intent);
+    }
+
+    private void registerResult(){
+        resultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        try{
+                            Uri imageUri = result.getData().getData();
+                            imageView.setImageURI(imageUri);
+                        }catch (Exception e){
+                            Toast.makeText(MainActivity.this,"No Image Selected", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+    }
+}
