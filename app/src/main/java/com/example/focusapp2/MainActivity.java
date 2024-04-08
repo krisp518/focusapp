d1def7370c720eb44bf500b68c38df9c399616c6
Ian Kim <eiankim518@gmail.com>
2024-04-08 05:51:32 -0700

    WORKING message function that allows user to input text that will be displayed once they click submit.
     There is also code for a notification function that is not yet working.

--- app/src/main/java/com/example/focusapp2/MainActivity.java
+++ app/src/main/java/com/example/focusapp2/MainActivity.java
@@ -5,25 +5,38 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
     //tickle me.
 
+    Button btnNotify;
     Button btnPickImage;
     ImageView imageView;
+    TextView message;
+    EditText inputMessage;
 
     ActivityResultLauncher<Intent> resultLauncher;
 
@@ -32,11 +45,47 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //image
         btnPickImage = findViewById(R.id.btnPickImage);
         imageView = findViewById(R.id.imageView);
         registerResult();
-
         btnPickImage.setOnClickListener(view -> pickImage());
+
+        //notification
+        btnNotify = findViewById(R.id.btnNotify);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My Notifications", "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        btnNotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //notification code
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "My Notifications");
+                builder.setContentTitle("my title");
+                builder.setContentText("lock in ong");
+                builder.setSmallIcon(R.drawable.ic_launcher_background);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+
+                //notification permission
+                /*if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[] { permission }, requestCode);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+                }*/
+                managerCompat.notify(1, builder.build());
+            }
+        });
+
+        //message
+        message = (TextView) findViewById(R.id.message);
+        inputMessage = (EditText) findViewById(R.id.inputMessage);
     }
 
     private void pickImage(){
@@ -60,4 +109,13 @@ public class MainActivity extends AppCompatActivity{
                 }
         );
     }
+
+    public void updateMessage(View view){
+        message.setText("\"" + inputMessage.getText() + "\"");
+        inputMessage.setText(" ");
+    }
+
+
+
+
 }
